c47e67bd2b3bfa24788012ccccc407f8aacf37cd
Liam Ellis <liamhellis@gmail.com>
2017-03-24 00:49:09 -0400

    home page static styles, have to replace with dynamic content from cosmicjs API

--- src/App.js
+++ src/App.js
@@ -15,9 +15,8 @@ class App extends Component {
       <div className="App">
         <Header />
         <Home />
-        <Blog />
+    
         <Footer />
-        <BlogPost />
       </div>
     );
   }
